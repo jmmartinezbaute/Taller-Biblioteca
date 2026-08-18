0fe24bceeaf93b5b78b82e3efa435d82011c0a8c
jesus <jmmartinezbaute@unicesar.edu.co>
2026-08-17 21:19:44 -0500

    feat: crear clase Material como padre de Libro

--- taller-Biblioteca/src/main/java/com/mycompany/taller/biblioteca/Cliente.java
+++ taller-Biblioteca/src/main/java/com/mycompany/taller/biblioteca/Cliente.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.taller.biblioteca;
 
-/**
- *
- * @author HP
- */
-public class Cliente {
+public class Cliente extends Persona {
+
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(String email, String nombre, String telefono, String id) {
+        super(nombre, telefono, id);
+        this.email = email;
+    }
     
+
+    public Cliente(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- taller-Biblioteca/src/main/java/com/mycompany/taller/biblioteca/Persona.java
+++ taller-Biblioteca/src/main/java/com/mycompany/taller/biblioteca/Persona.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.taller.biblioteca;
 
-/**
- *
- * @author HP
- */
 public class Persona {
-    
+
+    private String nombre;
+    private String telefono;
+    private String id;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String telefono, String id) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
