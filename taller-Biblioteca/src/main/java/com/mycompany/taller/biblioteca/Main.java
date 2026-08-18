1cc6bc015530f47a2d27527406605fd9b3c8564e
jesus <jmmartinezbaute@unicesar.edu.co>
2026-08-17 21:10:48 -0500

    feat: implementar CREATE de Cliente

--- taller-Biblioteca/src/main/java/com/mycompany/taller/biblioteca/Main.java
+++ taller-Biblioteca/src/main/java/com/mycompany/taller/biblioteca/Main.java
@@ -1,16 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.taller.biblioteca;
 
-/**
- *
- * @author HP
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+    static ArrayList<Cliente> clientes = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    }
+
+   
+    public static void crearCliente() {
+
+        System.out.println("=== CREAR CLIENTE ===");
+
+        System.out.print("Ingrese el ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Ingrese el nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese el teléfono: ");
+        String telefono = sc.nextLine();
+
+        System.out.print("Ingrese el email: ");
+        String email = sc.nextLine();
+
+        Cliente cliente = new Cliente(email, nombre, telefono, id);
+
+        clientes.add(cliente);
+
+        System.out.println("Cliente creado correctamente.");
     }
 }
