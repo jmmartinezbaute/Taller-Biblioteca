ab4791e18f996f7d89a11ea60736ed98216370b1
jesus <jmmartinezbaute@unicesar.edu.co>
2026-08-17 21:27:22 -0500

    feat: crear clase Libro heredando de Material

--- taller-Biblioteca/src/main/java/com/mycompany/taller/biblioteca/Material.java
+++ taller-Biblioteca/src/main/java/com/mycompany/taller/biblioteca/Material.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.taller.biblioteca;
 
-/**
- *
- * @author HP
- */
 public class Material {
+    private String codigo;
+    private String titulo;
+    private String aniopublic;
+
+    public Material() {
+    }
+
+    public Material(String codigo, String titulo, String aniopublic) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.aniopublic = aniopublic;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAniopublic() {
+        return aniopublic;
+    }
+
+    public void setAniopublic(String aniopublic) {
+        this.aniopublic = aniopublic;
+    }
     
 }
